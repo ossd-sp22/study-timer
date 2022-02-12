ebfba22c27190a6ab44cf0e8c0d57d581eb427fd
zhixuanevelynwu <zw1887@nyu.edu>
2022-02-11 19:31:25 -0500

    implemented pause and reset

--- popup/controls.js
+++ popup/controls.js
@@ -1,18 +1,35 @@
-let controls = document.getElementById("controls");
-let start_btn = document.getElementById("start");
-let pause_btn = document.getElementById("pause");
-let reset_btn = document.getElementById("reset");
-
-var duration = 30;
-startTimer(duration, container);
-
-var paused = false;
+localStorage.setItem("paused", false);
 pause_btn.onclick = function () {
-  if (!paused) {
+  var paused = localStorage.getItem("paused");
+  if (paused === "false") {
+    localStorage.setItem("paused", true);
     pause_btn.innerHTML = "Resume";
+    localStorage.setItem(
+      "duration",
+      Math.max(hours, 0) * 60 * 60 +
+        Math.max(minutes, 0) * 60 +
+        Math.max(seconds, 0)
+    );
+    container.removeChild(clock);
+    container.innerHTML = "Time paused";
+    localStorage.setItem("countDownDate", "none");
+    clearInterval(x);
   } else {
+    localStorage.setItem("paused", false);
     pause_btn.innerHTML = "Pause";
+    container.innerHTML = "";
+    clock = document.createElement("div");
+    container.appendChild(clock);
+    startTimer(Number(localStorage.getItem("duration")), clock);
   }
 };
 
-reset_btn.onclick = function () {};
+reset_btn.onclick = function () {
+  localStorage.setItem("countDownDate", "none");
+  localStorage.setItem("started", false);
+  container.removeChild(clock);
+  startBtn.style.display = "block";
+  timeInput.style.display = "block";
+  controls.style.display = "none";
+  clearInterval(x);
+};
--- popup/studyTimer.js
+++ popup/studyTimer.js
@@ -1,47 +1,74 @@
 let container = document.getElementById("timer");
 let debug = document.getElementById("debug");
-let startBtn = document.getElementById('start');
-let timeInput = document.getElementById('duration');
+let startBtn = document.getElementById("start");
+let timeInput = document.getElementById("duration");
+let controls = document.getElementById("controls");
+let start_btn = document.getElementById("start");
+let pause_btn = document.getElementById("pause");
+let reset_btn = document.getElementById("reset");
+var clock;
+var duration = 30;
 
-let duration;
+if (
+  !localStorage.getItem("started") ||
+  localStorage.getItem("started") === "false"
+) {
+  startBtn.style.display = "block";
+  timeInput.style.display = "block";
+  controls.style.display = "none";
+} else {
+  startBtn.style.display = "none";
+  timeInput.style.display = "none";
+  clock = document.createElement("div");
+  container.appendChild(clock);
+  startTimer(Number(localStorage.getItem("duration")), clock);
+}
 
-startBtn.addEventListener('click', () => {
-  startBtn.style.display = 'none';
-  timeInput.style.display = 'none';
-  time_string = document.getElementById('duration').value;
-  [h, m, s] = time_string.split(':');
-  duration = Number(h) *60 *60 + Number(m) * 60 + Number(s);
-  startTimer(duration, container);
+startBtn.addEventListener("click", () => {
+  time_string = document.getElementById("duration").value;
+  if (time_string && time_string.trim() != "") {
+    startBtn.style.display = "none";
+    timeInput.style.display = "none";
+    container.style.display = "block";
+    controls.style.display = "block";
+    localStorage.setItem("countDownDate", "none");
+    [h, m, s] = time_string.split(":");
+    duration = Number(h) * 60 * 60 + Number(m) * 60 + Number(s);
+    clock = document.createElement("div");
+    container.appendChild(clock);
+    startTimer(duration, clock);
+    localStorage.setItem("started", true);
+  }
 });
 
-function startTimer(duration, container) {
+var hours, minutes, seconds;
+function startTimer(duration, clock) {
   var d = new Date().getTime();
-  var countDownDate = new Date(d + duration * 60000); // add duration minutes to cdt
-  if (!localStorage.getItem("countDownDate")) {
+  var countDownDate = new Date(d + (duration + 2) * 1000); // add duration minutes to cdt
+  if (
+    !localStorage.getItem("countDownDate") ||
+    localStorage.getItem("countDownDate") == "none"
+  ) {
     localStorage.setItem("countDownDate", countDownDate);
   } else {
     countDownDate = new Date(localStorage.getItem("countDownDate")).getTime();
   }
 
-  setInterval(function () {
+  var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    container.innerHTML =
-      hours + "h " + minutes + "m " + seconds + "s ";
+    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    clock.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
 
     if (distance < 0) {
       clearInterval(x);
-      container.innerHTML = "EXPIRED";
-      startBtn.style.display = 'block';
-      timeInput.style.display = 'block';
+      localStorage.setItem("countDownDate", "none");
+      container.removeChild(clock);
+      startBtn.style.display = "block";
+      timeInput.style.display = "block";
+      controls.style.display = "none";
     }
   }, 1000);
 }
